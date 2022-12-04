9ee11139832046c7db0987fe6e9cd8642ef82cff
SaadSamee-123 <saadsameequreshi@gmail.com>
2022-12-05 00:45:03 +0500

    updated

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const app = express();
 const http = require("http");
@@ -10,7 +9,7 @@ app.use(express.json());
 const userRoute = require("./routes/userRoute");
 const adminRoute = require("./routes/adminRoute");
 const doctorRoute = require("./routes/doctorsRoute");
-const path = require("path");
+// const path = require("path");
 
 app.get("/", (_, res) => {
   res.status(200).send("Online");
@@ -20,13 +19,13 @@ app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/doctor", doctorRoute);
 
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use("/", express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+//   });
+// }
 const port = process.env.PORT || 8000;
 
 // app.get("/", (req, res) => res.send("Hello World!"));
